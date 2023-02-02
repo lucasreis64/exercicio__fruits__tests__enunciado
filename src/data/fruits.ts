b96ca9bcaa7fb5fe0a1c2a00904fb01238176d25
Lucas <lucasreis64@gmail.com>
2023-02-02 14:23:35 -0300

    feat: beforeAll, beforeEach & afterAll

--- src/data/fruits.ts
+++ src/data/fruits.ts
@@ -1,21 +1,5 @@
 import { Fruit } from "../repositories/fruits-repository";
 
-const fruits: Fruit[] = [
-  {
-      id: 1,
-      name: "maçã",
-      price: 5
-  },
-  {
-    id: 2,
-    name: "banana",
-    price: 3
-  },
-  {
-    id: 3,
-    name: "manga",
-    price: 8
-}
-];
+const fruits: Fruit[] = [];
 
 export default fruits;
--- tests/fruits.test.ts
+++ tests/fruits.test.ts
@@ -1,14 +1,42 @@
 import app from "../src/index";
 import supertest from "supertest";
-import fruitsRepository from "repositories/fruits-repository";
+import fruitsRepository, { Fruit } from "repositories/fruits-repository";
+import fruits from "data/fruits";
 
 const api = supertest(app);
 
+function clearData() {
+    while (fruits.length > 0) fruits.shift();
+}
+
 describe("GET: /fruits", () => {
-    it("should respond with status 200 and ticket data", async () => {
-        //supertest faz a requisição
+
+    beforeAll(() => {
+        clearData();
+        const maça: Fruit = {
+            id: 1,
+            name: "maçã",
+            price: 5,
+        };
+        const banana: Fruit = {
+            id: 2,
+            name: "banana",
+            price: 3,
+        };
+        const manga: Fruit = {
+            id: 3,
+            name: "manga",
+            price: 8,
+        };
+        fruits.push(maça, banana, manga);
+    });
+
+    afterAll(clearData);
+
+    it("should respond with status 200 and fruits data", async () => {
+
         const result = await api.get("/fruits");
-        //matcher para status code
+
         expect(result.status).toBe(200);
 
         expect(result.body).toEqual(
@@ -24,25 +52,49 @@ describe("GET: /fruits", () => {
 });
 
 describe("GET: /fruits/:id", () => {
-    it("should respond with status 200 and ticket data", async () => {
-        //supertest faz a requisição
-        const result = await api.get("/fruits/3");
-        //matcher para status code
+    beforeAll(() => {
+        clearData();
+        const maça: Fruit = {
+            id: 1,
+            name: "maçã",
+            price: 5,
+        };
+
+        fruits.push(maça);
+    });
+    afterAll(clearData);
+
+    it("should respond with status 200 and fruits data", async () => {
+
+        const result = await api.get("/fruits/1");
+
         expect(result.status).toBe(200);
 
-        expect(result.body).toEqual(
-            expect.objectContaining({
-                id: expect.any(Number),
-                name: expect.any(String),
-                price: expect.any(Number),
-            })
-        );
+        expect(result.body).toStrictEqual(fruits[0]);
+    });
+
+    it("should respond with status 404 if id doesn't exists on the database", async () => {
+        const result = await api.get("/fruits/10");
+        expect(result.status).toBe(404);
     });
 });
 
 describe("POST: /fruits/", () => {
-    it("should respond with status 201 and it should insert the new fruit in the database", async () => {
-        //supertest faz a requisição
+    beforeEach(clearData);
+    afterAll(clearData);
+
+    it("should respond with status 422 when body is not valid", async () => {
+      const body = {
+        test: 'testando tudo'
+      };
+
+      const result = await api.post("/fruits").send(body);
+
+      expect(result.status).toBe(422);
+    });
+
+    it("should respond with status 201 and insert the new fruit in the database", async () => {
+
         const fruitsLengthBefore = fruitsRepository.getFruits().length;
 
         const fruitBody = {
@@ -52,11 +104,32 @@ describe("POST: /fruits/", () => {
 
         const result = await api.post("/fruits").send(fruitBody);
 
+        expect(result.status).toBe(201);
+
         const fruitsLengthAfter = fruitsRepository.getFruits().length;
 
         expect(fruitsLengthAfter).toBe(fruitsLengthBefore + 1);
 
         const newFruit = fruitsRepository.getFruits()[fruitsLengthAfter - 1];
         expect(newFruit).toMatchObject(fruitBody);
+    });
+
+    it("should respond with status 401 doesn't insert the new fruit in the database", async () => {
+        const maça: Fruit = {
+            id: 1,
+            name: "maçã",
+            price: 5,
+        };
+
+        fruits.push(maça);
+
+        const fruitBody = {
+            name: "maçã",
+            price: 5,
+        };
+
+        const result = await api.post("/fruits").send(fruitBody);
+
+        expect(result.status).toBe(409);
     });
 });
